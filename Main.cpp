b022bf4598cd72528c9c46d5fecf09462f47b49b
AcTh2 <68511655+acth2@users.noreply.github.com>
2022-11-03 23:55:23 +0100

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,4 @@
-//Début: 03/11/22  Heure: 01:28
+//DÃ©but: 03/11/22  Heure: 01:28
 //Fin: ???
 //AcTh2
 
@@ -17,7 +17,7 @@
 //###########################################
 
 // conditions: ==, !=, >=, <=, < >, &&, ||
-// \_ Ce qui va utilisé ces conditions sont plus générallement les << while, for, if, .... >>
+// \_ Ce qui va utilisÃ© ces conditions sont plus gÃ©nÃ©rallement les << while, for, if, .... >>
 // le if est un peu pres comme le java:
 
 //
@@ -28,33 +28,36 @@
 
 //###########################################
 
-// Ce qui en est des for c'est aussi un peu pres comme le java (générallement une variable, une instruction, et la facon de faire cette instruction)
-// A toutes s'attentes pour ajouté <1> a une variable ce n'est pas i++ mais ++i !
-// 
+// Ce qui en est des for c'est aussi un peu pres comme le java (gÃ©nÃ©rallement une variable, une instruction, et la facon de faire cette instruction)
+// A toutes s'attentes pour ajoutÃ© <1> a une variable ce n'est pas i++ mais ++i !
+//
 // for (int i = 0; i <= 10; ++i
 // {
 //		std::cout << i << std::endl;
 // }
 // std::cin.ignore();
 //
-// 
+//
 // La boucle while est aussi un peu pres comme le java !
 // Exemple:
-// 
+//
 // while(true)
 // {
 // 		std::cout << "Comme le java !" << std::endl;
-// } 
+// }
 // 	std::cin.ignore();
-// 
+//
 //
 
 #include <iostream>
 
+//specifie que tu utilise std
+using namespace std;
+
 int main()
 {
-	int a = NULL;
-	int b = NULL;
+	int a;
+	int b;
 	int age = 13;
 	char name[] = "";
 	int end_count = 0;
@@ -63,77 +66,66 @@ int main()
 
 	//===================================================================
 
-	std::cout << "Quelle est ton nom : " << std::endl;
-	std::cin >> name;
-	std::cin.ignore();
+	cout << "Hello, World \n\n";
 
-	std::cout << "Quelle age a tu : " << std::endl;
-	std::cin >> age;
-	std::cin.ignore();
+	cout<< "Quelle est ton nom : ";
+	cin >> name;
+	cin.ignore();
 
-	std::cout << "Tu t'appelle: " << name << " et" << std::endl;
-	std::cout << "Tu as: " << age << " ans" << std::endl;
-	std::cin.ignore();
+	cout << "Quelle age a tu : ";
+	cin >> age;
+	cin.ignore();
 
-	//===================================================================
-
-	std::cout << "Variable A et B: ";
-    std::cin >> a >> b;
-	std::cin.ignore();
-
-	if (a == NULL || b == NULL)
-	{
-		std::cout << "Il s'agirais d'ecrire.." << std::endl;
-
-
-		std::cout << "Le resultat est: rien?" << std::endl;
-		std::cin.ignore();
-	}
-	else
-	{
-		std::cout << "Le resultat est: " << a + b << std::endl;
-		std::cin.ignore();
-	}
+	cout << "Tu t'appelle: " << name << " et" << " tu as " << age << " ans! \n";
+	cin.ignore();
 
 	//===================================================================
 
-	std::cout << "Si le nombre est 0 ou moins alors le retour seras 1 sinon 0 : " << std::endl;
-	std::cin >> firstif;
+	cout << "Variable A et B: ";
+    	cin >> a >> b;
+	cin.ignore();
+	cout << "Le resultat est: " << a + b << "\n";
+	cin.ignore();
+
+	//===================================================================
+
+	cout << "Si le nombre est 0 ou moins alors le retour seras 1 sinon 0 :";
+	cin >> firstif;
 
 	if (firstif <= 0)
 	{
-		std::cout << "1" << std::endl;
-		std::cin.ignore();
+		cout << "1 \n" << std::endl;
+		cin.ignore();
 	}
 	else
 	{
-		std::cout << "0" << std::endl;
-		std::cin.ignore();
+		cout << "0\n" << std::endl;
+		cin.ignore();
 	}
-	
-	std::cout << "\n" << std::endl;
-	std::cout << "Compte de 0 a 10 avec la boucle for" << std::endl;
-	std::cout << "Jusqu'a combien le programme va compter?:" << std::endl;
-	std::cin >> end_count;
-	std::cin.ignore();
+
+	cout << "\n" << std::endl;
+	cout << "Compte de 0 a 10 avec la boucle for\n" << std::endl;
+	cout << "Jusqu'a combien le programme va compter?:" << std::endl;
+	cin >> end_count;
+	cin.ignore();
 
 	for (int i = 0; i <= end_count; ++i)
 	{
 		std::cout << i << std::endl;
-    }
-	std::cin.ignore();
-	std::cout << "Le programme a compte jusqu'a " << end_count << " !" << std::endl;
-	std::cin.ignore();
+        }
+	cin.ignore();
+	cout << "Le programme a compte jusqu'a " << end_count << " ! \n" << std::endl;
+	cin.ignore();
 
-	std::cout << "Merci de mettre la table de multiplication que vous voulez.. " << std::endl;
-	std::cin >> table;
-	std::cin.ignore();
-	std::cout << "Voici la table de " << table << std::endl;
+	cout << "Merci de mettre la table de multiplication que vous voulez.. " << std::endl;
+	cin >> table;
+	cin.ignore();
+	cout << "Voici la table de " << table << "\n" << std::endl;
 	for (int ib = 0; ib <= 10; ++ib)
 	{
-		std::cout << ib * table << std::endl;
+		cout << ib * table << "\n";
 	}
-	std::cin.ignore();
+	cin.ignore();
 
 	//===================================================================
 
